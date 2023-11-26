3d4515066e4f57a1a84e722d11c49f35d7972101
nayemoya <107650526+nayemoya@users.noreply.github.com>
2023-11-26 01:40:01 -0600

    holiii

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,21 @@
-
-    // Función para mostrar el modal al hacer clic en la tarjeta
-    function abrirModal(idModal) {
-      document.getElementById(idModal).style.display = 'flex';
-      // Agregar un event listener para cerrar el modal al hacer clic fuera de él
-      window.addEventListener('click', function(event) {
-        const modal = document.getElementById(idModal);
-        if (event.target === modal) {
-          modal.style.display = 'none';
-        }
-      });
+// Función para mostrar el modal al hacer clic en la tarjeta
+function abrirModal(idModal) {
+  document.getElementById(idModal).style.display = 'flex';
+  // Agregar un event listener para cerrar el modal al hacer clic fuera de él
+  window.addEventListener('click', function(event) {
+    const modal = document.getElementById(idModal);
+    if (event.target === modal) {
+      modal.style.display = 'none';
     }
+  });
+
+  document.addEventListener('keydown', function (event) {
+    var modal = document.getElementById(idModal);
+    if (event.key === 'Escape') {
+        modal.style.display = 'none';
+    }
+});
+}
   
     function cerrarModal(idModal) {
       document.getElementById(idModal).style.display = 'none';
